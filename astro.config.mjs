73d561880758abac261e32e3d317faff83e29766
Hector Ventura Alvarez <hectorvenalvarez@gamil.com>
2026-05-27 01:59:39 +0200

    Forzar despliegue con GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   site: 'https://Alien0Dev.github.io',
-  base: 'AgenciaMV'
+  // Usamos un operador ternario para que solo aplique el base en producción
+  base: process.env.NODE_ENV === 'production' ? '/AgenciaMV' : '/'
 });
