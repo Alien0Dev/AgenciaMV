8d1c4475b2cb595f3266ab13ecc78f50cd5363ac
Hector Ventura Alvarez <hectorvenalvarez@gamil.com>
2026-05-27 01:29:52 +0200

    agregado soporte github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  site: 'https://Alien0Dev.github.io',
+  base: 'AgenciaMV'
 });
